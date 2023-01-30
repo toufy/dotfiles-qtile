e3a7809f9e39ce16bb39236c3660d9f4c6419502
toufy <116110494+toufy@users.noreply.github.com>
2023-01-30 16:42:18 +0200

    unnecessary configs

--- scripts/color-scripts/randomcolor.py
+++
@@ -1,9 +0,0 @@
-#!/bin/python3
-import pathlib
-import random
-import os
-import subprocess
-homedir = os.getenv("HOME")
-curr_file = pathlib.Path(__file__).parent.resolve()
-script = random.choice([i for i in os.listdir(curr_file) if i != 'randomcolor.py'])
-subprocess.run(f"{homedir}/scripts/color-scripts/{script}")
